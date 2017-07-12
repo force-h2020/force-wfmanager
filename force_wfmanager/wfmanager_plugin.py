2c894202c208ba01906ba34025f2767627dc482d
martinRenou <martin.renou@isae.fr>
2017-07-12 17:26:43 +0100

    Add extension points for mco kpi and constraint

--- force_wfmanager/wfmanager_plugin.py
+++ force_wfmanager/wfmanager_plugin.py
@@ -1,21 +1,45 @@
-from envisage.api import Plugin
+from envisage.api import Plugin, ExtensionPoint
 from envisage.ui.tasks.api import TaskFactory
-from traits.api import List
+from traits.api import List, Instance
 
 
 class WfManagerPlugin(Plugin):
     """ The basic WfManager """
 
-    # PREFERENCES = 'envisage.preferences'
-    # PREFERENCES_PANES = 'envisage.ui.tasks.preferences_panes'
     TASKS = 'envisage.ui.tasks.tasks'
+
+    MCOS = 'force_wfmanager.mcos'
+    KPIS = 'force_wfmanager.kpis'
+    CONSTRAINTS = 'force_wfmanager.constraints'
 
     id = 'force_wfmanager.wfmanager_plugin'
     name = 'Workflow Manager'
 
-    # preferences = List(contributes_to=PREFERENCES)
-    # preferences_panes = List(contributes_to=PREFERENCES_PANES)
     tasks = List(contributes_to=TASKS)
+
+    mcos = ExtensionPoint(
+        List(Instance('force_wfmanager.spec.mco.IMCO')),
+        id=MCOS,
+        desc="""
+        Available MCOs for the Workflow Manager
+        """
+    )
+
+    kpis = ExtensionPoint(
+        List(Instance('force_wfmanager.spec.kpi.IKPI')),
+        id=KPIS,
+        desc="""
+        Available KPIs for the Workflow Manager
+        """
+    )
+
+    constraints = ExtensionPoint(
+        List(Instance('force_wfmanager.spec.constraint.IConstraint')),
+        id=KPIS,
+        desc="""
+        Defined constraints for the Workflow Manager
+        """
+    )
 
     def _tasks_default(self):
         from force_wfmanager.wfmanager_task import WfManagerTask
