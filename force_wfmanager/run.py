273fc78d1e053511bfbec516c86b6700e4fec1bb
martinRenou <martin.renou@isae.fr>
2017-07-14 09:49:17 +0100

    Add stevedore for plugin imports

--- force_wfmanager/run.py
+++ force_wfmanager/run.py
@@ -1,18 +1,29 @@
 from envisage.core_plugin import CorePlugin
 from envisage.ui.tasks.tasks_plugin import TasksPlugin
 
-from force_bdss.data_sources.data_sources_plugin import (
-    DataSourcesPlugin)
-from force_bdss.mco.multi_criteria_optimizers_plugin import (
-    MultiCriteriaOptimizersPlugin)
+from stevedore import extension
+from stevedore.exception import NoMatches
 
 from force_wfmanager.wfmanager import WfManager
 from force_wfmanager.wfmanager_plugin import WfManagerPlugin
 
 
 def main():
-    plugins = [CorePlugin(), TasksPlugin(), WfManagerPlugin(),
-               DataSourcesPlugin(),
-               MultiCriteriaOptimizersPlugin()]
+    plugins = [CorePlugin(), TasksPlugin(), WfManagerPlugin()]
+
+    mgr = extension.ExtensionManager(
+        namespace='force.bdss.extensions',
+        invoke_on_load=True
+    )
+
+    def import_extensions(ext):
+        print("Found extension {}".format(ext.name))
+        plugins.append(ext.obj)
+
+    try:
+        mgr.map(import_extensions)
+    except NoMatches:
+        print("No extensions found")
+
     wfmanager = WfManager(plugins=plugins)
     wfmanager.run()
