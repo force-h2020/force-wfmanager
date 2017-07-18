a2e47201e4775e3634f24201c738c11a708b0fc8
martinRenou <martin.renou@isae.fr>
2017-07-18 09:37:24 +0100

    Add adapters for the tree, and create the Workflow class

--- force_wfmanager/left_side_pane/workflow_settings.py
+++ force_wfmanager/left_side_pane/workflow_settings.py
@@ -1,12 +1,45 @@
 from pyface.tasks.api import TraitsDockPane
-from traitsui.api import ListStrEditor, Tabbed, UItem, View
+from traitsui.api import (ListStrEditor, Tabbed, UItem, View, TreeNode,
+                          ITreeNodeAdapter, ITreeNode)
 from traitsui.list_str_adapter import ListStrAdapter
-from traits.api import Any, List
+from traits.api import Any, adapts, Instance, List, HasTraits
+
+from force_bdss.mco.i_multi_criteria_optimizer_bundle import (
+    IMultiCriteriaOptimizerBundle)
+from force_bdss.data_sources.i_data_source_bundle import IDataSourceBundle
+from force_bdss.kpi.i_kpi_calculator_bundle import IKPICalculatorBundle
 
 
 class ListAdapter(ListStrAdapter):
     def get_text(self, object, trait, index):
         return self.item.name
+
+
+class McoAdapter(ITreeNodeAdapter):
+    adapts(IMultiCriteriaOptimizerBundle, ITreeNode)
+
+    def get_label(self):
+        return self.adaptee.name
+
+
+class DataSourceAdapter(ITreeNodeAdapter):
+    adapts(IDataSourceBundle, ITreeNode)
+
+    def get_label(self):
+        return self.adaptee.name
+
+
+class KpiCalculatorAdapter(ITreeNodeAdapter):
+    adapts(IKPICalculatorBundle, ITreeNode)
+
+    def get_label(self):
+        return self.adaptee.name
+
+
+class Workflow(HasTraits):
+    mco = Instance(IMultiCriteriaOptimizerBundle)
+    data_sources = List(Instance(IDataSourceBundle))
+    kpi_calculators = List(Instance(IKPICalculatorBundle))
 
 
 class WorkflowSettings(TraitsDockPane):
