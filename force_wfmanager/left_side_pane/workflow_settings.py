e1c21e6b5064a9f7fb15d250da5222c5ce135bae
martinRenou <martin.renou@isae.fr>
2017-07-18 14:15:05 +0100

    Use ModelView class for elements of the Workflow

--- force_wfmanager/left_side_pane/workflow_settings.py
+++ force_wfmanager/left_side_pane/workflow_settings.py
@@ -1,8 +1,9 @@
 from pyface.tasks.api import TraitsDockPane
 from traitsui.api import (ListStrEditor, ITreeNodeAdapter, ITreeNode, Tabbed,
-                          TreeEditor, TreeNode, UItem, VGroup, View)
+                          TreeEditor, TreeNode, UItem, VGroup, View, ModelView)
 from traitsui.list_str_adapter import ListStrAdapter
-from traits.api import adapts, Instance, List, HasTraits
+from traits.api import (adapts, Button, Instance, List,
+                        on_trait_change, HasTraits, Str)
 
 from force_bdss.mco.base_mco_model import BaseMCOModel
 from force_bdss.data_sources.base_data_source_model import BaseDataSourceModel
@@ -21,39 +22,78 @@ class ListAdapter(ListStrAdapter):
         return self.item.name
 
 
+class MCOModelView(ModelView):
+    name = Str()
+
+    model = Instance(BaseMCOModel)
+
+    view = View(
+        UItem('model', style='custom')
+    )
+
+
+class DataSourceModelView(ModelView):
+    name = Str()
+
+    model = Instance(BaseDataSourceModel)
+
+    view = View(
+        UItem('model', style='custom')
+    )
+
+
+class KPICModelView(ModelView):
+    name = Str()
+
+    model = Instance(BaseKPICalculatorModel)
+
+    view = View(
+        UItem('model', style='custom')
+    )
+
+
 class McoAdapter(ITreeNodeAdapter):
-    """ Adapts the MCO model to be displayed in the tree
+    """ Adapts the MCO model view to be displayed in the tree
     """
-    adapts(BaseMCOModel, ITreeNode)
+    adapts(MCOModelView, ITreeNode)
 
     def get_label(self):
         return self.adaptee.name
+
+    def get_view(self):
+        return self.adaptee.trait_view()
 
 
 class DataSourceAdapter(ITreeNodeAdapter):
-    """ Adapts the Data source model to be displayed in the tree
+    """ Adapts the Data source model view to be displayed in the tree
     """
-    adapts(BaseDataSourceModel, ITreeNode)
+    adapts(DataSourceModelView, ITreeNode)
 
     def get_label(self):
         return self.adaptee.name
+
+    def get_view(self):
+        return self.adaptee.trait_view()
 
 
 class KpiCalculatorAdapter(ITreeNodeAdapter):
     """ Adapts the KPI calculator model to be displayed in the tree
     """
-    adapts(BaseKPICalculatorModel, ITreeNode)
+    adapts(KPICModelView, ITreeNode)
 
     def get_label(self):
         return self.adaptee.name
+
+    def get_view(self):
+        return self.adaptee.trait_view()
 
 
 class Workflow(HasTraits):
     """ Definition of the workflow
     """
-    mco = List(Instance(BaseMCOModel))
-    data_sources = List(Instance(BaseDataSourceModel))
-    kpi_calculators = List(Instance(BaseKPICalculatorModel))
+    mco = List(Instance(MCOModelView))
+    data_sources = List(Instance(DataSourceModelView))
+    kpi_calculators = List(Instance(KPICModelView))
 
 
 # Create an empty view for objects that have no data to display:
@@ -71,7 +111,6 @@ tree_editor = TreeEditor(
                  auto_open=True,
                  children='mco',
                  label='=MCO',
-                 view=no_view,
                  ),
         TreeNode(node_for=[Workflow],
                  auto_open=True,
@@ -104,6 +143,10 @@ class WorkflowSettings(TraitsDockPane):
     selected_data_source = Instance(IDataSourceBundle)
     selected_kpi_calculator = Instance(IKPICalculatorBundle)
 
+    add_mco_button = Button("Add")
+    add_data_source_button = Button("Add")
+    add_kpi_calculator_button = Button("Add")
+
     workflow = Instance(Workflow)
 
     view = View(VGroup(
@@ -111,31 +154,66 @@ class WorkflowSettings(TraitsDockPane):
               editor=tree_editor,
               show_label=False),
         Tabbed(
-            UItem(
-                "available_mcos",
-                editor=ListStrEditor(
-                    adapter=ListAdapter(),
-                    selected="selected_mco"),
+            VGroup(
+                UItem(
+                    name='add_mco_button'
+                ),
+                UItem(
+                    "available_mcos",
+                    editor=ListStrEditor(
+                        adapter=ListAdapter(),
+                        selected="selected_mco"),
+                ),
                 label='MCOs',
             ),
-            UItem(
-                'available_data_sources',
-                editor=ListStrEditor(
-                    adapter=ListAdapter(),
-                    selected="selected_data_source"),
-                label='Data Sources'
+            VGroup(
+                UItem(
+                    name='add_data_source_button'
+                ),
+                UItem(
+                    "available_data_sources",
+                    editor=ListStrEditor(
+                        adapter=ListAdapter(),
+                        selected="selected_data_source"),
+                ),
+                label='Data Sources',
             ),
-            UItem(
-                'available_kpi_calculators',
-                editor=ListStrEditor(
-                    adapter=ListAdapter(),
-                    selected="selected_kpi_calculator"),
+            VGroup(
+                UItem(
+                    name='add_kpi_calculator_button'
+                ),
+                UItem(
+                    'available_kpi_calculators',
+                    editor=ListStrEditor(
+                        adapter=ListAdapter(),
+                        selected="selected_kpi_calculator")
+                ),
                 label='KPI Calculators'
-            ))
-        ),
+            ),
+        )),
         width=800,
         height=600,
         resizable=True)
 
     def _workflow_default(self):
         return Workflow()
+
+    @on_trait_change('add_mco_button')
+    def add_mco(self):
+        self.workflow.mco[0] = MCOModelView(
+            name=self.selected_mco.name,
+            model=self.selected_mco.create_model()),
+
+    @on_trait_change('add_data_source_button')
+    def add_data_source(self):
+        self.workflow.data_sources.append(DataSourceModelView(
+            name=self.selected_data_source.name,
+            model=self.selected_data_source.create_model(),
+        ))
+
+    @on_trait_change('add_kpi_calculator_button')
+    def add_kpi_calculator(self):
+        self.workflow.kpi_calculators.append(KPICModelView(
+            name=self.selected_kpi_calculator.name,
+            model=self.selected_kpi_calculator.create_model(),
+        ))
