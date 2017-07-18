8a863eaef050970bef27ecc00c0b5b761e594ab5
martinRenou <martin.renou@isae.fr>
2017-07-18 12:55:55 +0100

    Review

--- force_wfmanager/left_side_pane/workflow_settings.py
+++ force_wfmanager/left_side_pane/workflow_settings.py
@@ -3,6 +3,10 @@ from traitsui.api import ListStrEditor, Tabbed, UItem, View
 from traitsui.list_str_adapter import ListStrAdapter
 from traits.api import List, Instance
 
+from force_bdss.mco.i_multi_criteria_optimizer_bundle import (
+    IMultiCriteriaOptimizerBundle)
+from force_bdss.data_sources.i_data_source_bundle import IDataSourceBundle
+
 
 class ListAdapter(ListStrAdapter):
     def get_text(self, object, trait, index):
@@ -16,10 +20,8 @@ class WorkflowSettings(TraitsDockPane):
     available_mcos = List()
     available_data_sources = List()
 
-    selected_mco = Instance('force_bdss.mco.i_multi_criteria_optimizer_bundle.'
-                            'IMultiCriteriaOptimizerBundle')
-    selected_data_source = Instance('force_bdss.data_sources.'
-                                    'i_data_source_bundle.IDataSourceBundle')
+    selected_mco = Instance(IMultiCriteriaOptimizerBundle)
+    selected_data_source = Instance(IDataSourceBundle)
 
     view = View(Tabbed(
         UItem(
