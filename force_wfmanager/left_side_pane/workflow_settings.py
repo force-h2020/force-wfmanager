672e903754b4fa689966e127b6ebf8a51f3c1c56
martinRenou <martin.renou@isae.fr>
2017-07-20 10:14:16 +0100

    Add Workflow ModelView which permit to display MCO

--- force_wfmanager/left_side_pane/workflow_settings.py
+++ force_wfmanager/left_side_pane/workflow_settings.py
@@ -1,9 +1,9 @@
 from pyface.tasks.api import TraitsDockPane
 from traitsui.api import (ListStrEditor, ITreeNodeAdapter, ITreeNode, Tabbed,
-                          TreeEditor, TreeNode, UItem, VGroup, View)
+                          TreeEditor, TreeNode, UItem, VGroup, View, ModelView)
 from traitsui.list_str_adapter import ListStrAdapter
 from traits.api import (adapts, Button, Instance, List,
-                        on_trait_change)
+                        on_trait_change, Property, property_depends_on)
 
 from force_bdss.api import (
     BaseMCOModel, BaseDataSourceModel, BaseKPICalculatorModel,
@@ -54,26 +54,60 @@ class KpiCalculatorAdapter(ITreeNodeAdapter):
         return get_bundle_name(self.adaptee.bundle)
 
 
+class WorkflowModelView(ModelView):
+    model = Instance(Workflow)
+
+    mco_representation = Property(List(Instance(BaseMCOModel)))
+    data_sources_representation = Property(List(Instance(
+        BaseDataSourceModel)))
+    kpi_calculators_representation = Property(List(Instance(
+        BaseKPICalculatorModel)))
+
+    @property_depends_on('model.multi_criteria_optimizer', settable=False)
+    def _get_mco_representation(self):
+        if self.model.multi_criteria_optimizer is not None:
+            return [self.model.multi_criteria_optimizer]
+        else:
+            return []
+
+    @property_depends_on('model.data_sources', settable=False)
+    def _get_data_sources_representation(self):
+        return self.model.data_sources
+
+    @property_depends_on('model.kpi_calculators', settable=False)
+    def _get_kpi_calculators_representation(self):
+        return self.model.kpi_calculators
+
+    def _model_default(self):
+        return Workflow()
+
+
 # Create an empty view for objects that have no data to display:
 no_view = View()
 
 tree_editor = TreeEditor(
     nodes=[
-        TreeNode(node_for=[Workflow],
+        TreeNode(node_for=[WorkflowModelView],
                  auto_open=True,
                  children='',
                  label='=Workflow',
                  view=no_view
                  ),
-        TreeNode(node_for=[Workflow],
+        TreeNode(node_for=[WorkflowModelView],
                  auto_open=True,
-                 children='data_sources',
+                 children='mco_representation',
+                 label='=MCO',
+                 view=no_view
+                 ),
+        TreeNode(node_for=[WorkflowModelView],
+                 auto_open=True,
+                 children='data_sources_representation',
                  label='=Data sources',
                  view=no_view
                  ),
-        TreeNode(node_for=[Workflow],
+        TreeNode(node_for=[WorkflowModelView],
                  auto_open=True,
-                 children='kpi_calculators',
+                 children='kpi_calculators_representation',
                  label='=KPI calculators',
                  view=no_view
                  ),
@@ -109,7 +143,7 @@ class WorkflowSettings(TraitsDockPane):
     add_data_source_button = Button("Add")
     add_kpi_calculator_button = Button("Add")
 
-    workflow = Instance(Workflow)
+    workflow = Instance(WorkflowModelView)
 
     view = View(VGroup(
         UItem(name='workflow',
@@ -158,21 +192,21 @@ class WorkflowSettings(TraitsDockPane):
         resizable=True)
 
     def _workflow_default(self):
-        return Workflow()
+        return WorkflowModelView()
 
     @on_trait_change('add_mco_button')
     def add_mco(self):
-        self.workflow.multi_criteria_optimizer = \
+        self.workflow.model.multi_criteria_optimizer = \
             self.selected_mco.create_model()
 
     @on_trait_change('add_data_source_button')
     def add_data_source(self):
-        self.workflow.data_sources.append(
+        self.workflow.model.data_sources.append(
             self.selected_data_source.create_model()
         )
 
     @on_trait_change('add_kpi_calculator_button')
     def add_kpi_calculator(self):
-        self.workflow.kpi_calculators.append(
+        self.workflow.model.kpi_calculators.append(
             self.selected_kpi_calculator.create_model()
         )
