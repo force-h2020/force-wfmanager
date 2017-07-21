790a80117b88f08489e5642002c9599e67a8033e
martinRenou <martin.renou@isae.fr>
2017-07-21 09:44:59 +0100

    Add test for get_bundle_name function

--- test/test_workflow_settings.py
+++ test/test_workflow_settings.py
@@ -1,14 +1,40 @@
 import unittest
 
-from force_wfmanager.left_side_pane.workflow_settings import WorkflowSettings
+from traits.api import String
 
 from force_bdss.core_plugins.dummy_mco.dakota.dakota_bundle import DakotaBundle
 from force_bdss.core_plugins.csv_extractor.csv_extractor.csv_extractor_bundle \
     import CSVExtractorBundle
 from force_bdss.core_plugins.dummy_kpi.kpi_adder.kpi_adder_bundle import (
     KPIAdderBundle)
-from force_bdss.api import (BaseMCOModel, BaseDataSourceModel,
-                            BaseKPICalculatorModel)
+
+from force_bdss.api import (
+    BaseMCOModel, BaseDataSourceModel, BaseKPICalculatorModel,
+    BaseDataSourceBundle)
+
+from force_wfmanager.left_side_pane.workflow_settings import (
+    WorkflowSettings, get_bundle_name)
+
+
+class NamedBundle(BaseDataSourceBundle):
+    id = String('enthought.test.bundle.named')
+    name = String('   Really cool bundle  ')
+
+    def create_data_source(self, application, model):
+        pass
+
+    def create_model(self, model_data=None):
+        pass
+
+
+class UnnamedBundle(BaseDataSourceBundle):
+    id = String('enthought.test.bundle.unnamed')
+
+    def create_data_source(self, application, model):
+        pass
+
+    def create_model(self, model_data=None):
+        pass
 
 
 class TestWorkflowSettings(unittest.TestCase):
@@ -119,3 +145,12 @@ class TestWorkflowSettings(unittest.TestCase):
 
         self.assertEqual(
             len(self.settings.workflow.kpi_calculators_representation), 2)
+
+    def test_get_bundle_name(self):
+        named_bundle = NamedBundle()
+        self.assertEqual(
+            get_bundle_name(named_bundle), 'Really cool bundle')
+
+        unnamed_bundle = UnnamedBundle()
+        self.assertEqual(
+            get_bundle_name(unnamed_bundle), 'enthought.test.bundle.unnamed')
