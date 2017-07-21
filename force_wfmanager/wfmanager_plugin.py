75d290e16d4c7151f1cf431ef916110093298627
martinRenou <martin.renou@isae.fr>
2017-07-21 11:36:08 +0100

    Use force_bdss ids

--- force_wfmanager/wfmanager_plugin.py
+++ force_wfmanager/wfmanager_plugin.py
@@ -2,15 +2,13 @@ from envisage.api import Plugin, ExtensionPoint
 from envisage.ui.tasks.api import TaskFactory
 from traits.api import List, Instance
 
+from force_bdss.ids import ExtensionPointID
+
 
 class WfManagerPlugin(Plugin):
     """ The basic WfManager """
 
     TASKS = 'envisage.ui.tasks.tasks'
-
-    MCO_BUNDLES = 'force.bdss.mco.bundles'
-    DATA_SOURCE_BUNDLES = 'force.bdss.data_sources.bundles'
-    KPI_CALCULATOR_BUNDLES = 'force.bdss.kpi_calculators.bundles'
 
     id = 'force_wfmanager.wfmanager_plugin'
     name = 'Workflow Manager'
@@ -20,7 +18,7 @@ class WfManagerPlugin(Plugin):
     mco_bundles = ExtensionPoint(
         List(Instance('force_bdss.mco.i_multi_criteria_optimizer_bundle.'
                       'IMultiCriteriaOptimizerBundle')),
-        id=MCO_BUNDLES,
+        id=ExtensionPointID.MCO_BUNDLES,
         desc="""
         Available MCOs for the Workflow Manager
         """
@@ -29,7 +27,7 @@ class WfManagerPlugin(Plugin):
     data_source_bundles = ExtensionPoint(
         List(Instance('force_bdss.data_sources.i_data_source_bundle.'
                       'IDataSourceBundle')),
-        id=DATA_SOURCE_BUNDLES,
+        id=ExtensionPointID.DATA_SOURCE_BUNDLES,
         desc="""
         Available Datasources for the Workflow Manager
         """
@@ -38,7 +36,7 @@ class WfManagerPlugin(Plugin):
     kpi_calculator_bundles = ExtensionPoint(
         List(Instance('force_bdss.kpi.i_kpi_calculator_bundle.'
                       'IKPICalculatorBundle')),
-        id=KPI_CALCULATOR_BUNDLES,
+        id=ExtensionPointID.KPI_CALCULATOR_BUNDLES,
         desc="""
         Available KPI calculators for the Workflow Manager
         """
