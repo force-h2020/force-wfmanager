0eea7531a88315285e1ec195b2af645c81848711
martinRenou <martin.renou@isae.fr>
2017-07-25 09:49:14 +0100

    Remove irrelevant tests

--- test/test_workflow_settings.py
+++ test/test_workflow_settings.py
@@ -49,10 +49,6 @@ class TestWorkflowSettings(unittest.TestCase):
         self.workflow = self.settings.workflow.model
 
     def test_ui_initialization(self):
-        self.assertIsNone(self.settings.selected_mco)
-        self.assertIsNone(self.settings.selected_data_source)
-        self.assertIsNone(self.settings.selected_kpi_calculator)
-
         self.assertIsNone(self.workflow.multi_criteria_optimizer)
         self.assertEqual(len(self.workflow.data_sources), 0)
         self.assertEqual(len(self.workflow.kpi_calculators), 0)
@@ -62,89 +58,6 @@ class TestWorkflowSettings(unittest.TestCase):
             len(self.settings.workflow.data_sources_representation), 0)
         self.assertEqual(
             len(self.settings.workflow.kpi_calculators_representation), 0)
-
-    def test_add_multi_criteria_optimizer(self):
-        # Simulate pressing add mco button (should do nothing)
-        self.settings.add_mco()
-        self.assertIsNone(self.workflow.multi_criteria_optimizer)
-
-        # Simulate selecting an mco bundle in the list
-        self.settings.selected_mco = self.settings.available_mcos[0]
-
-        # Simulate pressing add mco button
-        self.settings.add_mco()
-        self.assertIsInstance(
-            self.workflow.multi_criteria_optimizer,
-            BaseMCOModel)
-        old_mco = self.workflow.multi_criteria_optimizer
-
-        # Simulate pressing add mco button again to create a new mco model
-        self.settings.add_mco()
-        self.assertNotEqual(
-            self.workflow.multi_criteria_optimizer,
-            old_mco)
-
-        self.assertEqual(len(self.settings.workflow.mco_representation), 1)
-
-    def test_add_data_source(self):
-        # Simulate pressing add data_source button (should do nothing)
-        self.settings.add_data_source()
-        self.assertEqual(len(self.workflow.data_sources), 0)
-
-        # Simulate selecting a data_source bundle in the list
-        self.settings.selected_data_source = \
-            self.settings.available_data_sources[0]
-
-        # Simulate pressing add data_source button
-        self.settings.add_data_source()
-        self.assertIsInstance(
-            self.workflow.data_sources[0],
-            BaseDataSourceModel)
-
-        self.assertEqual(
-            len(self.settings.workflow.data_sources_representation), 1)
-
-        # Simulate pressing add data_source button again
-        self.settings.add_data_source()
-        self.assertIsInstance(
-            self.workflow.data_sources[1],
-            BaseDataSourceModel)
-        self.assertNotEqual(
-            self.workflow.data_sources[0],
-            self.workflow.data_sources[1])
-
-        self.assertEqual(
-            len(self.settings.workflow.data_sources_representation), 2)
-
-    def test_add_kpi_calculator(self):
-        # Simulate pressing add kpi_calculator button (should do nothing)
-        self.settings.add_kpi_calculator()
-        self.assertEqual(len(self.workflow.kpi_calculators), 0)
-
-        # Simulate selecting a kpi_calculator bundle in the list
-        self.settings.selected_kpi_calculator = \
-            self.settings.available_kpi_calculators[0]
-
-        # Simulate pressing add kpi_calculator button
-        self.settings.add_kpi_calculator()
-        self.assertIsInstance(
-            self.workflow.kpi_calculators[0],
-            BaseKPICalculatorModel)
-
-        self.assertEqual(
-            len(self.settings.workflow.kpi_calculators_representation), 1)
-
-        # Simulate pressing add kpi_calculator button again
-        self.settings.add_kpi_calculator()
-        self.assertIsInstance(
-            self.workflow.kpi_calculators[1],
-            BaseKPICalculatorModel)
-        self.assertNotEqual(
-            self.workflow.kpi_calculators[0],
-            self.workflow.kpi_calculators[1])
-
-        self.assertEqual(
-            len(self.settings.workflow.kpi_calculators_representation), 2)
 
     def test_get_bundle_name(self):
         named_bundle = NamedBundle()
