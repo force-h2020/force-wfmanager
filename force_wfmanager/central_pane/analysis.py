b7528788ba5e4e2995d1720228c6138670b53afa
martinRenou <martin.renou@isae.fr>
2017-08-01 13:23:55 +0100

    Add tabbs

--- force_wfmanager/central_pane/analysis.py
+++ force_wfmanager/central_pane/analysis.py
@@ -1,9 +1,24 @@
 from pyface.tasks.api import TraitsTaskPane
-from traitsui.api import View
+
+from traits.api import Int
+
+from traitsui.api import View, Tabbed, VGroup, Item
 
 
 class Analysis(TraitsTaskPane):
     id = 'force_wfmanager.analysis'
     name = 'Analysis'
 
-    view = View()
+    test1 = Int()
+    test2 = Int()
+
+    view = View(Tabbed(
+        VGroup(
+            Item('test1'),
+            label='Pareto Front'
+        ),
+        VGroup(
+            Item('test2'),
+            label='Results'
+        )
+    ))
