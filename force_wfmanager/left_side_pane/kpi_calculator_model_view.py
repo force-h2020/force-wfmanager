14435ba9a28e9bf871e2f10767c12549104bbf54
martinRenou <martin.renou@isae.fr>
2017-08-07 15:14:00 +0100

    Create first version of KPICalculatorModelView

---
+++ force_wfmanager/left_side_pane/kpi_calculator_model_view.py
@@ -0,0 +1,43 @@
+from traits.api import Instance, Str, List, Property, Dict
+
+from traitsui.api import View, Item, ModelView
+
+from force_bdss.api import BaseKPICalculatorModel
+
+from .view_utils import get_factory_name
+
+
+class KPICalculatorModelView(ModelView):
+    #: KPI Calculator model
+    model = Instance(BaseKPICalculatorModel, allow_none=False)
+
+    #: The human readable name of the KPI Calculator
+    name = Str()
+
+    #: The input slot maps which is a list of dictionary defining the input
+    #: slots of the KPI Calculator model
+    input_slot_maps = Property(
+        List(Dict),
+        depends_on='model.input_slot_maps'
+    )
+
+    #: The output list defining the output names of the KPI Calculator
+    output_slot_names = Property(
+        List(Str),
+        depends_on='model.output_slot_names'
+    )
+
+    traits_view = View(
+        Item(name="input_slot_maps"),
+        Item(name="output_slot_names"),
+        kind="subpanel",
+    )
+
+    def _name_default(self):
+        return get_factory_name(self.model.factory)
+
+    def _get_input_slot_maps(self):
+        return self.model.input_slot_maps
+
+    def _get_output_slot_names(self):
+        return self.model.output_slot_names
