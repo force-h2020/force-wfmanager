3804837b8d5cdabaed982b6869c0b54bab0c15f7
martinRenou <martin.renou@isae.fr>
2017-08-09 13:03:36 +0100

    Changed plugin for the tests

--- test/test_evaluator_model_view.py
+++ test/test_evaluator_model_view.py
@@ -8,29 +8,33 @@ from traits.api import Instance
 
 from envisage.plugin import Plugin
 
-from force_bdss.core_plugins.dummy.csv_extractor.csv_extractor_data_source import CSVExtractorDataSource  # noqa
-from force_bdss.core_plugins.dummy.csv_extractor.csv_extractor_factory import \
-    CSVExtractorFactory
+from force_bdss.core_plugins.dummy.kpi_adder.kpi_adder_calculator import \
+    KPIAdderCalculator
+from force_bdss.core_plugins.dummy.kpi_adder.kpi_adder_factory import \
+    KPIAdderFactory
+from force_bdss.core.input_slot_map import InputSlotMap
 
 from force_wfmanager.left_side_pane.evaluator_model_view import \
     EvaluatorModelView
 
 
 class BadEvaluatorModelView(EvaluatorModelView):
-    model = Instance(CSVExtractorFactory)
+    model = Instance(KPIAdderFactory)
 
 
 class EvaluatorModelViewTest(unittest.TestCase):
     def setUp(self):
-        self.model = CSVExtractorFactory(mock.Mock(spec=Plugin)).create_model()
+        self.model = KPIAdderFactory(mock.Mock(spec=Plugin)).create_model()
+        self.evaluator = KPIAdderFactory(
+            mock.Mock(spec=Plugin)).create_kpi_calculator()
 
         self.evaluator_mv = EvaluatorModelView(model=self.model)
 
     def test_evaluator_model_view_init(self):
-        self.assertEqual(self.evaluator_mv.label, "CSV Extractor")
+        self.assertEqual(self.evaluator_mv.label, "KPI Adder")
         self.assertIsInstance(
             self.evaluator_mv._evaluator,
-            CSVExtractorDataSource)
+            KPIAdderCalculator)
         self.assertEqual(len(self.evaluator_mv.output_slots_representation), 1)
         self.assertEqual(self.model.output_slot_names[0], '')
 
@@ -42,4 +46,22 @@ class EvaluatorModelViewTest(unittest.TestCase):
         with self.assertRaisesRegexp(TypeError, "The EvaluatorModelView needs "
                                                 "a BaseDataSourceModel"):
             BadEvaluatorModelView(
-                model=CSVExtractorFactory(mock.Mock(spec=Plugin)))
+                model=KPIAdderFactory(mock.Mock(spec=Plugin)))
+
+    def test_bad_input_slots(self):
+        input_slots, _ = self.evaluator.slots(self.model)
+
+        self.model.input_slot_maps = [
+            InputSlotMap(name='') for input_slot in range(len(input_slots) + 1)
+        ]
+
+        with self.assertRaisesRegexp(RuntimeError, "input slots"):
+            EvaluatorModelView(model=self.model)
+
+    def test_bad_output_slots(self):
+        _, output_slots = self.evaluator.slots(self.model)
+
+        self.model.output_slot_names = (len(output_slots) + 1)*['']
+
+        with self.assertRaisesRegexp(RuntimeError, "output slots"):
+            EvaluatorModelView(model=self.model)
