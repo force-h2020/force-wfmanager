dd0857fce80ae589846fb918e187ba1ef2d54cd4
martinRenou <martin.renou@isae.fr>
2017-08-09 15:36:14 +0100

    Removes redundant test

--- test/test_plot.py
+++ test/test_plot.py
@@ -23,31 +23,6 @@ class PlotTest(unittest.TestCase):
         self.analysis_model.value_names = ['density', 'pressure']
         self.assertEqual(self.plot.data_arrays, [[], []])
 
-    def test_data_arrays(self):
-        self.analysis_model.value_names = ['density', 'pressure']
-        self.analysis_model.evaluation_steps = [
-            (1.010, 101325),
-            (1.100, 101423),
-            (1.123, 102000),
-            (1.156, 102123),
-            (1.242, 102453),
-        ]
-
-        self.assertEqual(len(self.plot.data_arrays), 2)
-
-        self.assertEqual(len(self.plot.data_arrays[0]), 5)
-        self.assertEqual(len(self.plot.data_arrays[1]), 5)
-
-        self.assertEqual(
-            self.plot.data_arrays[0],
-            [1.010, 1.100, 1.123, 1.156, 1.242]
-        )
-
-        self.assertEqual(
-            self.plot.data_arrays[1],
-            [101325, 101423, 102000, 102123, 102453]
-        )
-
     def test_plot(self):
         self.analysis_model.value_names = ['density', 'pressure']
         self.analysis_model.evaluation_steps = [
