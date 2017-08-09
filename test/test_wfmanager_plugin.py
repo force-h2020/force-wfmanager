2573336b88bde96e3a67fbc208abe0671054505a
Stefano Borini <stefanoborini@users.noreply.github.com>
2017-08-09 13:46:09 +0100

    Update test_wfmanager_plugin.py

--- test/test_wfmanager_plugin.py
+++ test/test_wfmanager_plugin.py
@@ -8,8 +8,6 @@ from envisage.api import Application
 
 from force_wfmanager.wfmanager_plugin import WfManagerPlugin
 
-WFMANAGER_TASK_PATH = "force_wfmanager.wfmanager_plugin.WfManagerTask"
-
 
 class WfManagerPluginTest(unittest.TestCase):
     def setUp(self):
@@ -23,8 +21,11 @@ class WfManagerPluginTest(unittest.TestCase):
 
         def mock_wfmanager_task_constructor(*args, **kwargs):
             return
+        
 
-        with mock.patch(WFMANAGER_TASK_PATH) as mock_task:
+        with mock.patch(
+                "force_wfmanager.wfmanager_plugin"
+                ".WfManagerTask") as mock_task:
             mock_task.side_effect = mock_wfmanager_task_constructor
 
             self.wfmanager_plugin._create_task()
