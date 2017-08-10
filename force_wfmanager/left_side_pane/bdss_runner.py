7bb7dd43c51eec51bff7790cdf15a87ce5bed961
martinRenou <martin.renou@isae.fr>
2017-08-10 14:54:22 +0100

    Add behavior of the run button

--- force_wfmanager/left_side_pane/bdss_runner.py
+++ force_wfmanager/left_side_pane/bdss_runner.py
@@ -1,6 +1,8 @@
+import os
+
 from pyface.tasks.api import TraitsDockPane
 
-from traits.api import Instance, Button
+from traits.api import Instance, Button, on_trait_change
 
 from traitsui.api import View, UItem
 
@@ -18,3 +20,12 @@ class BDSSRunner(TraitsDockPane):
     traits_view = View(
         UItem('run_button')
     )
+
+    @on_trait_change('run_button')
+    def run(self):
+        self.wfmanager_task.save_workflow()
+
+        if len(self.wfmanager_task.current_file) == 0:
+            raise RuntimeError("Can not run if you do not save the workflow")
+
+        os.system("force_bdss {}".format(self.wfmanager_task.current_file))
