0beb6a83b742d7ddad3eb4e9f7b100420f7fed50
Stefano Borini <sborini@enthought.com>
2017-08-11 23:29:51 +0100

    Coverage

--- force_wfmanager/plugins/ui_notification/ui_notification_plugin.py
+++ force_wfmanager/plugins/ui_notification/ui_notification_plugin.py
@@ -2,7 +2,7 @@ from force_bdss.api import BaseExtensionPlugin, plugin_id
 from .ui_notification_factory import UINotificationFactory
 
 
-class DummyPlugin(BaseExtensionPlugin):
+class UINotificationPlugin(BaseExtensionPlugin):
     id = plugin_id("enthought", "ui_notification")
 
     def _notification_listener_factories_default(self):
