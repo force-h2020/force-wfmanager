f52b1edc62349bca1b2263b6306641d3b78b696e
Stefano Borini <sborini@enthought.com>
2017-08-11 22:33:59 +0100

    Moved UI notification plugin to the workflow manager

---
+++ force_wfmanager/plugins/ui_notification/ui_notification_plugin.py
@@ -0,0 +1,9 @@
+from force_bdss.api import BaseExtensionPlugin, plugin_id
+from .ui_notification_factory import UINotificationFactory
+
+
+class DummyPlugin(BaseExtensionPlugin):
+    id = plugin_id("enthought", "ui_notification")
+
+    def _notification_listener_factories_default(self):
+        return [UINotificationFactory(self)]
