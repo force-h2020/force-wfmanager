5c7a339fc0f514c4408ae969490971388b17c02d
Stefano Borini <sborini@enthought.com>
2017-08-15 18:17:34 +0100

    Changed sockets and state names

--- force_wfmanager/zmq_server.py
+++ force_wfmanager/zmq_server.py
@@ -8,61 +8,71 @@ log = logging.getLogger(__name__)
 
 
 class ZMQServer(threading.Thread):
-    def __init__(self, analysis_model):
+
+    (STATE_STOPPED,
+     STATE_WAITING,
+     STATE_RECEIVING) = range(3)
+
+    def __init__(self, config, analysis_model):
         super(ZMQServer, self).__init__()
         self.daemon = True
-        self.context = zmq.Context()
+        self.config = config
+        self._context = zmq.Context()
         self.analysis_model = analysis_model
+        self.state = ZMQServer.STATE_STOPPED
 
     def run(self):
-        # Socket to talk to server
-        print("hello")
-        context = self.context
-        sub_socket = context.socket(zmq.SUB)
-        sub_socket.bind("tcp://*:12345")
-        sub_socket.setsockopt(zmq.SUBSCRIBE, "".encode("utf-8"))
-        sub_socket.setsockopt(zmq.LINGER, 0)
+        if self.state != ZMQServer.STATE_STOPPED:
+            return
 
-        sync_socket = context.socket(zmq.REP)
-        sync_socket.setsockopt(zmq.LINGER, 0)
-        sync_socket.bind("tcp://*:12346")
+        # Socket to talk to server
+        log.info("Server started")
+
+        pub_socket, rep_socket = self._setup_sockets()
 
         poller = zmq.Poller()
-        poller.register(sub_socket)
-        poller.register(sync_socket)
+        poller.register(pub_socket)
+        poller.register(rep_socket)
 
-        WAITING = 0
-        RECEIVING = 1
-
-        state = WAITING
+        state = ZMQServer.STATE_WAITING
 
         while True:
-            print("polling")
             events = dict(poller.poll())
-            print(events)
-            if sync_socket in events:
-                data = sync_socket.recv_string()
-                print("received ", data)
+            if rep_socket in events:
+                data = rep_socket.recv_string()
                 if data.startswith("HELLO\n"):
-                    sync_socket.send_string(data)
-                    state = RECEIVING
+                    rep_socket.send_string(data)
+                    state = ZMQServer.STATE_RECEIVING
                 elif data.startswith("GOODBYE\n"):
-                    sync_socket.send_string(data)
-                    state = WAITING
+                    rep_socket.send_string(data)
+                    state = ZMQServer.STATE_WAITING
                 else:
-                    print("unknown request", data)
+                    log.error("Unknown request received {}".format(data))
 
-            if sub_socket in events:
-                if state == RECEIVING:
-                    string = sub_socket.recv_string()
+            if pub_socket in events:
+                if state == ZMQServer.STATE_RECEIVING:
+                    string = pub_socket.recv_string()
                     split_data = string.split("\n")
                     if split_data[2] == "MCO_PROGRESS":
                         self._deliver_info(split_data[3:])
                     elif split_data[2] == "MCO_START":
                         self._reset_info()
                 else:
-                    print("data while waiting. discarding")
-                    string = sub_socket.recv_string()
+                    log.error("Received data while waiting. Discarding")
+                    string = pub_socket.recv_string()
+
+    def _setup_sockets(self):
+        context = self._context
+        pub_socket = context.socket(zmq.SUB)
+        pub_socket.bind(self.config.pub_socket_url)
+        pub_socket.setsockopt(zmq.SUBSCRIBE, "".encode("utf-8"))
+        pub_socket.setsockopt(zmq.LINGER, 0)
+
+        rep_socket = context.socket(zmq.REP)
+        rep_socket.setsockopt(zmq.LINGER, 0)
+        rep_socket.bind(self.config.rep_socket_url)
+
+        return pub_socket, rep_socket
 
     def _deliver_info(self, data):
         def _add_data():
