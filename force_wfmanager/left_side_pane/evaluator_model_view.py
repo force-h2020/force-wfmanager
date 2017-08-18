b417127e7daeb2a78e7018f425cde0b1290b7ac9
martinRenou <martin.renou@isae.fr>
2017-08-18 13:15:28 +0100

    Clear diff

--- force_wfmanager/left_side_pane/evaluator_model_view.py
+++ force_wfmanager/left_side_pane/evaluator_model_view.py
@@ -24,7 +24,7 @@ class TableRow(HasStrictTraits):
     model = Either(
         Instance(BaseDataSourceModel),
         Instance(BaseKPICalculatorModel),
-        allow_none=False
+        allow_none=False,
     )
 
 
