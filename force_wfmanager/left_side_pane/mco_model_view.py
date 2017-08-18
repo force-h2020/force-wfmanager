fad10d9c39a716236ffbb263ec9c5726f70b631f
martinRenou <martin.renou@isae.fr>
2017-08-18 12:15:14 +0100

    Clear MCOModelView code

--- force_wfmanager/left_side_pane/mco_model_view.py
+++ force_wfmanager/left_side_pane/mco_model_view.py
@@ -1,7 +1,7 @@
 from traits.api import Instance, List, Str, on_trait_change
 from traitsui.api import ModelView
 
-from force_bdss.api import BaseMCOModel, Identifier
+from force_bdss.api import BaseMCOModel
 
 from .mco_parameter_model_view import MCOParameterModelView
 from .view_utils import get_factory_name
@@ -17,24 +17,12 @@ class MCOModelView(ModelView):
     #: List of MCO parameters to be displayed in the TreeEditor
     mco_parameters_representation = List(Instance(MCOParameterModelView))
 
-    #: List of MCO parameter names
-    mco_parameters_names = List(Identifier)
-
     @on_trait_change('model.parameters[]')
     def update_mco_parameters_representation(self):
         """ Update the MCOParameterModelViews """
         self.mco_parameters_representation = [
             MCOParameterModelView(model=parameter)
             for parameter in self.model.parameters]
-
-    @on_trait_change('mco_parameters_representation.model.name')
-    def update_mco_parameter_names(self):
-        """ Update the available parameter names """
-        self.mco_parameters_names = [
-            p.model.name
-            for p in self.mco_parameters_representation
-            if len(p.model.name) != 0
-        ]
 
     def _label_default(self):
         return get_factory_name(self.model.factory)
