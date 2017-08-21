5eb6cc4d66f89365f0b1b3d11683b8624d51ab02
martinRenou <martin.renou@isae.fr>
2017-08-21 11:34:01 +0100

    Fix coverage

--- force_wfmanager/left_side_pane/tests/test_variable_names_register.py
+++ force_wfmanager/left_side_pane/tests/test_variable_names_register.py
@@ -14,6 +14,8 @@ from force_wfmanager.left_side_pane.mco_parameter_model_view import (
     MCOParameterModelView)
 from force_wfmanager.left_side_pane.data_source_model_view import (
     DataSourceModelView)
+from force_wfmanager.left_side_pane.kpi_calculator_model_view import (
+    KPICalculatorModelView)
 from force_wfmanager.left_side_pane.variable_names_register import (
     VariableNamesRegister)
 
@@ -51,18 +53,18 @@ def create_data_source_model():
 
 
 def create_kpi_model():
-    factory = mock.Mock(spec=BaseDataSourceFactory)
-    data_source = mock.Mock(spec=BaseDataSource)
+    factory = mock.Mock(spec=BaseKPICalculatorFactory)
+    kpi_calculator = mock.Mock(spec=BaseKPICalculator)
 
     def slots(*args, **kwargs):
         return (
             (Slot(type='P'), ),
             (Slot(type='T'), )
         )
-    data_source.slots = slots
+    kpi_calculator.slots = slots
 
-    factory.create_data_source = lambda: data_source
-    return DummyDataSourceModel(factory=factory)
+    factory.create_kpi_calculator = lambda: kpi_calculator
+    return DummyKPIModel(factory=factory)
 
 
 class VariableNamesRegisterTest(unittest.TestCase):
@@ -87,12 +89,20 @@ class VariableNamesRegisterTest(unittest.TestCase):
             DataSourceModelView(model=self.data_source2),
         ]
 
+        self.kpi1 = create_kpi_model()
+        self.kpi2 = create_kpi_model()
+
+        self.register.kpi_calculators_mv = [
+            KPICalculatorModelView(model=self.kpi1),
+            KPICalculatorModelView(model=self.kpi2),
+        ]
+
     def test_register_init(self):
         self.assertEqual(len(self.register.mco_parameters_mv), 3)
         self.assertEqual(len(self.register.mco_parameters_names), 0)
         self.assertEqual(len(self.register.data_sources_mv), 2)
         self.assertEqual(len(self.register.data_sources_output_names), 0)
-        self.assertEqual(len(self.register.kpi_calculators_mv), 0)
+        self.assertEqual(len(self.register.kpi_calculators_mv), 2)
 
     def test_available_names_update(self):
         self.assertEqual(self.register.mco_parameters_names, [])
@@ -110,10 +120,20 @@ class VariableNamesRegisterTest(unittest.TestCase):
         self.param1.name = ''
         self.assertEqual(self.register.mco_parameters_names,
                          ['V2', 'V3'])
+
         self.assertEqual(self.register.data_sources_mv[0].available_variables,
                          ['V2', 'V3'])
+        self.assertEqual(
+            self.register.kpi_calculators_mv[0].available_variables,
+            ['V2', 'V3'])
 
         self.data_source1.output_slot_names = ['T1']
         self.data_source2.output_slot_names = ['T2']
         self.assertEqual(self.register.data_sources_output_names,
                          ['T1', 'T2'])
+
+        self.assertEqual(self.register.data_sources_mv[0].available_variables,
+                         ['V2', 'V3'])
+        self.assertEqual(
+            self.register.kpi_calculators_mv[0].available_variables,
+            ['V2', 'V3', 'T1', 'T2'])
