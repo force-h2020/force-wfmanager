ef726ad42f970873c059130d6198c93ff5bf387d
martinRenou <martin.renou@gmail.com>
2017-08-25 11:08:19 +0100

    Clean test_factory_name

--- force_wfmanager/left_side_pane/tests/test_factory_name.py
+++ force_wfmanager/left_side_pane/tests/test_factory_name.py
@@ -13,25 +13,21 @@ from force_bdss.api import BaseDataSourceFactory
 from force_wfmanager.left_side_pane.view_utils import get_factory_name
 
 
-class NamedFactory(BaseDataSourceFactory):
+class Factory(BaseDataSourceFactory):
+    def create_data_source(self, application, model):
+        pass
+
+    def create_model(self, model_data=None):
+        pass
+
+
+class NamedFactory(Factory):
     id = String('enthought.test.factory.named')
     name = String('   Really cool factory  ')
 
-    def create_data_source(self, application, model):
-        pass
 
-    def create_model(self, model_data=None):
-        pass
-
-
-class UnnamedFactory(BaseDataSourceFactory):
+class UnnamedFactory(Factory):
     id = String('enthought.test.factory.unnamed')
-
-    def create_data_source(self, application, model):
-        pass
-
-    def create_model(self, model_data=None):
-        pass
 
 
 class TestFactoryName(unittest.TestCase):
