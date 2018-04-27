aa12a0cc7ebd531ba2c86980f432fc2fe2bbe8cf
Stefano Borini <sborini@enthought.com>
2018-04-27 17:06:52 +0100

    Added comments and utility method to add a data source to the underlying model.

--- force_wfmanager/left_side_pane/execution_layer_model_view.py
+++ force_wfmanager/left_side_pane/execution_layer_model_view.py
@@ -5,8 +5,15 @@ from force_bdss.core.execution_layer import ExecutionLayer
 
 
 class ExecutionLayerModelView(ModelView):
+    #: The model object this MV wraps
     model = Instance(ExecutionLayer)
 
+    #: The label to display in the list
     label = String()
 
+    #: True if the wrapped object is valid.
     valid = Bool(True)
+
+    def add_data_source(self, data_source):
+        """Adds the passed data source model to the model data sources."""
+        self.model.data_sources.append(data_source)
