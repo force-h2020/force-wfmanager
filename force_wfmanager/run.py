42fb7223c651d63db7bef2c4499d97431746bbec
James Johnson <jjohnson@enthought.com>
2018-06-06 14:33:24 +0100

    Added --version command line arg via argparse

--- force_wfmanager/run.py
+++ force_wfmanager/run.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 from envisage.core_plugin import CorePlugin
 from envisage.ui.tasks.tasks_plugin import TasksPlugin
@@ -12,10 +13,21 @@ from force_wfmanager.wfmanager import WfManager
 from force_wfmanager.wfmanager_plugin import WfManagerPlugin
 
 from traits.api import push_exception_handler
+
+from .version import __version__
+
+
 push_exception_handler(lambda *args: None, reraise_exceptions=True)
 
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version',
+                        version='force_wfmanager version: '
+                                '{}'.format(__version__))
+    parser.parse_args()
+
     logging.basicConfig(filename="force_wfmanager.log", filemode="w")
     log = logging.getLogger(__name__)
 
