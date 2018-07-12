e5c446c318203719be83b4cfeeb4314dfca04616
Stefano Borini <sborini@enthought.com>
2018-07-12 21:23:05 +0100

    more refinements to the dialog including the HTML editor

--- force_wfmanager/plugin_dialog.py
+++ force_wfmanager/plugin_dialog.py
@@ -1,3 +1,4 @@
+from traitsui.editors import HTMLEditor
 from traitsui.list_str_adapter import ListStrAdapter
 
 from force_bdss.api import BaseExtensionPlugin
@@ -21,7 +22,7 @@ class PluginDialog(HasStrictTraits):
 
     selected_plugin = Instance(BaseExtensionPlugin)
 
-    selected_plugin_error = Str()
+    selected_plugin_message = Str()
 
     def __init__(self, plugins):
         super(PluginDialog, self).__init__(plugins=plugins)
@@ -29,12 +30,16 @@ class PluginDialog(HasStrictTraits):
     def default_traits_view(self):
         return View(
             HGroup(
-                UItem('plugins', editor=ListStrEditor(
-                    selected="selected_plugin",
-                    editable=False,
-                    adapter=PluginAdapter()
-                )),
-                UItem('selected_plugin_error', style="readonly"),
+                HGroup(
+                    UItem('plugins', editor=ListStrEditor(
+                        selected="selected_plugin",
+                        editable=False,
+                        adapter=PluginAdapter()
+                    )),
+                    UItem('selected_plugin_message',
+                          style="readonly",
+                          editor=HTMLEditor()),
+                ),
                 label="Plugins",
                 show_border=True
             )
@@ -42,9 +47,38 @@ class PluginDialog(HasStrictTraits):
 
     @on_trait_change('selected_plugin')
     def _sync_error(self):
-        print("XXX")
-        if self.selected_plugin is not None:
-            self.selected_plugin_error = self.selected_plugin.error_msg
+        plugin = self.selected_plugin
+        if plugin is None:
+            self.selected_plugin_message = htmlformat(
+                "Please select a plugin",
+                "")
             return
 
-        self.selected_plugin_error = "No errors for this plugin."
+        if plugin.broken:
+            self.selected_plugin_message = htmlformat("Plugin Error",
+                                                      plugin.error_msg)
+            return
+
+        self.selected_plugin_message = htmlformat("", "No errors for this plugin.")
+
+
+def htmlformat(title, message):
+    return """
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <style type="text/css">
+                .container{{
+                    width: 100%;
+                    display: block;
+                }}
+            </style>
+        </head>
+        <body>
+        <h1>{}</h1>
+        <p>{}</p>
+        </body>
+        </html>
+        """.format(title, message)
+
