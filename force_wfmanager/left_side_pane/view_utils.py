e727a460660111f6f6a1e63eb3c40fce9b9a2847
James Johnson <jjohnson@enthought.com>
2018-07-12 11:12:33 +0100

    whitespace

--- force_wfmanager/left_side_pane/view_utils.py
+++ force_wfmanager/left_side_pane/view_utils.py
@@ -51,7 +51,7 @@ def get_default_background_color():
     """Return the default background color for a Qt Application"""
     palette = QtGui.QPalette()
     rgba_color = palette.window().color().getRgb()
-    html_background_color = '#{:02x}{:02x}{:02x}'.format(rgba_color[0], 
+    html_background_color = '#{:02x}{:02x}{:02x}'.format(rgba_color[0],
                                                          rgba_color[1],
                                                          rgba_color[2])
     return html_background_color
