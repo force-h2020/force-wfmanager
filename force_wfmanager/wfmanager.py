38d26192ac7a60369477d0b1772e5815c89a6f07
James Johnson <jjohnson@enthought.com>
2018-07-24 17:14:23 +0100

    reversed order of save and application_exit event

--- force_wfmanager/wfmanager.py
+++ force_wfmanager/wfmanager.py
@@ -12,3 +12,15 @@ class WfManager(TasksApplication):
             active_task='force_wfmanager.wfmanager_task',
             size=(800, 600)
         )]
+
+    def _prepare_exit(self):
+        """Same functionality as TasksApplication._prepare_exit(), but
+        _save_state is called before application_exiting is fired"""
+        self._save_state()
+        self.application_exiting = self
+
+    def _application_exiting_fired(self):
+        for window in self.windows:
+            tasks = window.tasks
+            for task in tasks:
+                window.remove_task(task)
