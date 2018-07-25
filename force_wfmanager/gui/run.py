94dac1bbf20cd793c9def5d4f216fc8661e3d14b
James Johnson <jjohnson@enthought.com>
2018-07-25 13:12:37 +0100

    Added debug logging option (--debug)

--- force_wfmanager/gui/run.py
+++ force_wfmanager/gui/run.py
@@ -7,13 +7,12 @@ from envisage.ui.tasks.tasks_plugin import TasksPlugin
 from stevedore import extension
 from stevedore.exception import NoMatches
 
+from traits.api import push_exception_handler
+
 from force_bdss.api import FactoryRegistryPlugin
 
 from force_wfmanager.wfmanager import WfManager
 from force_wfmanager.wfmanager_plugin import WfManagerPlugin
-
-from traits.api import push_exception_handler
-
 from force_wfmanager.version import __version__
 
 push_exception_handler(lambda *args: None, reraise_exceptions=True)
@@ -21,17 +20,24 @@ push_exception_handler(lambda *args: None, reraise_exceptions=True)
 
 @click.command()
 @click.version_option(version=__version__)
+@click.option('--debug', is_flag=True, default=False,
+              help="Prints extra debug information in force_wfmanager.log")
 @click.argument('workflow_file', type=click.Path(exists=True), required=False,
                 default=None,)
-def force_wfmanager(workflow_file):
+def force_wfmanager(workflow_file, debug):
     """Launches the FORCE workflow manager application"""
-    main(workflow_file=workflow_file)
+    main(workflow_file=workflow_file, debug=debug)
 
 
-def main(workflow_file=None):
+def main(workflow_file=None, debug=False):
     """Launches the FORCE workflow manager application"""
-    logging.basicConfig(filename="force_wfmanager.log", filemode="w")
+    if debug is False:
+        logging.basicConfig(filename="force_wfmanager.log", filemode="w")
+    else:
+        logging.basicConfig(filename="force_wfmanager.log", filemode="w",
+                            level=logging.DEBUG)
     log = logging.getLogger(__name__)
+
     plugins = [CorePlugin(), TasksPlugin(), FactoryRegistryPlugin(),
                WfManagerPlugin(workflow_file=workflow_file)]
 
