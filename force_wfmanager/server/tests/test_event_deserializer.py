e6370fa98a7c75daf0dfb0bea97deae70c474ae7
Stefano Borini <sborini@enthought.com>
2018-08-06 13:42:15 +0100

    Coverage

--- force_wfmanager/server/tests/test_event_deserializer.py
+++ force_wfmanager/server/tests/test_event_deserializer.py
@@ -5,7 +5,7 @@ import unittest
 from force_wfmanager.server.event_deserializer import (
     EventDeserializer, DeserializerError)
 
-from force_bdss.api import MCOStartEvent
+from force_bdss.api import MCOStartEvent, MCOProgressEvent
 
 
 class TestEventDeserializer(unittest.TestCase):
@@ -22,6 +22,39 @@ class TestEventDeserializer(unittest.TestCase):
         self.assertIsInstance(event, MCOStartEvent)
         self.assertEqual(event.parameter_names, event.parameter_names)
         self.assertEqual(event.kpi_names, event.kpi_names)
+
+    def test_progress_event_deserialize(self):
+        data = json.dumps({
+            "type": "MCOProgressEvent",
+            "model_data": {
+                "optimal_point": [
+                    {
+                        "value": 1.0
+                    },
+                    {
+                        "value": 2.0
+                    }
+                ],
+                "optimal_kpis": [
+                    {
+                        "value": 3.0
+                    },
+                ],
+                "weights": [1.0],
+            }
+        })
+
+        event = EventDeserializer().deserialize(data)
+        self.assertIsInstance(event, MCOProgressEvent)
+        self.assertEqual(len(event.optimal_point), 2)
+        self.assertEqual(event.optimal_point[0].value, 1.0)
+        self.assertEqual(event.optimal_point[1].value, 2.0)
+
+        self.assertEqual(len(event.optimal_kpis), 1)
+        self.assertEqual(event.optimal_kpis[0].value, 3.0)
+
+        self.assertEqual(event.weights, [1.0])
+
 
     def test_invalid_cases(self):
         invalid_cases = [
