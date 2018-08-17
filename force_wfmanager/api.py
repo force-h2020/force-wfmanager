68cdbcb50d1ada27247f55ad8c25308dea8dd805
James Johnson <jjohnson@enthought.com>
2018-08-17 16:09:58 +0100

    api file for future use

---
+++ force_wfmanager/api.py
@@ -0,0 +1,36 @@
+from .TaskToggleGroupAccelerator import TaskToggleGroupAccelerator  # noqa
+
+from .version import __version__ as version  # noqa
+
+from .wfmanager import WfManager  # noqa
+
+from .wfmanager_plugin import WfManagerPlugin  # noqa
+
+from .plugin_dialog import PluginDialog  # noqa
+
+from .central_pane.analysis_model import AnalysisModel  # noqa
+from .central_pane.setup_pane import SetupPane  # noqa
+from .central_pane.result_table import ResultTable  # noqa
+
+from .left_side_pane.results_pane import ResultsPane  # noqa
+from .left_side_pane.tree_pane import TreePane  # noqa
+from .left_side_pane.data_source_model_view import DataSourceModelView  # noqa
+from .left_side_pane.variable_names_registry import VariableNamesRegistry  # noqa
+from .left_side_pane.kpi_specification_model_view import KPISpecificationModelView  # noqa
+from .left_side_pane.workflow_tree import WorkflowTree  # noqa
+from .left_side_pane.mco_model_view import MCOModelView  # noqa
+from .left_side_pane.execution_layer_model_view import ExecutionLayerModelView  # noqa
+from .left_side_pane.notification_listener_model_view import NotificationListenerModelView  # noqa
+from .left_side_pane.workflow_model_view import WorkflowModelView  # noqa
+from .left_side_pane.view_utils import model_info, get_default_background_color, get_factory_name  # noqa
+
+from .plugins.ui_notification.ui_notification import UINotification  # noqa
+from .plugins.ui_notification.ui_notification_model import UINotificationModel  # noqa
+from .plugins.ui_notification.ui_notification_hooks_manager import UINotificationHooksManager  # noqa
+from .plugins.ui_notification.ui_notification_hooks_factory import UINotificationHooksFactory  # noqa
+
+from .local_traits import ZMQSocketURL  # noqa
+
+from .server.zmq_server import ZMQServer  # noqa
+from .server.event_deserializer import EventDeserializer, DeserializerError  # noqa
+from .central_pane.plot import Plot  # noqa
