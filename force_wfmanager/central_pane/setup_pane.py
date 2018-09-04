5a0157deeddf472e13ee4bea5cd49489586a47ee
James Johnson <jjohnson@enthought.com>
2018-09-04 13:18:38 +0100

    Removed unused analysis_model in setup pane

--- force_wfmanager/central_pane/setup_pane.py
+++ force_wfmanager/central_pane/setup_pane.py
@@ -1,26 +1,21 @@
 from pyface.tasks.api import TraitsTaskPane
 
-from traits.api import Instance, Dict
+from traits.api import Dict
 
 from traitsui.api import View, VGroup, UItem
 from traitsui.editors import ShellEditor
-
-from .analysis_model import AnalysisModel
 
 
 class SetupPane(TraitsTaskPane):
     id = 'force_wfmanager.setup_pane'
     name = 'Setup Pane'
 
-    #: The model for the analysis part
-    analysis_model = Instance(AnalysisModel)
-
     # Namespace for the console
     console_ns = Dict()
 
     view = View(VGroup(
             UItem("console_ns", label="Console", editor=ShellEditor()),
-    ),
+        ),
     )
 
     def __init__(self, *args, **kwargs):
