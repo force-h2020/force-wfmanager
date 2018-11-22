dbb88d84a3e2fb00e9e74246ae388cc3d645c446
James Johnson <jjohnson@enthought.com>
2018-11-22 15:31:45 +0000

    Updated to match the default objective being minimise

--- force_wfmanager/left_side_pane/kpi_specification_model_view.py
+++ force_wfmanager/left_side_pane/kpi_specification_model_view.py
@@ -75,7 +75,5 @@ class KPISpecificationModelView(ModelView):
         """Gets the label from the model object"""
         if self.model.name == '':
             return "KPI"
-        elif self.model.objective == '':
-            return "KPI: {}".format(self.model.name)
 
         return "KPI: {} ({})".format(self.model.name, self.model.objective)
