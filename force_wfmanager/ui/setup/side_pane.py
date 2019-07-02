d42b4d530c0b895e4750e7071134dda728df14d7
flongford <flongford@enthought.com>
2019-07-02 19:31:09 +0100

    CLN: removed error reporting from side_pane and replaced Workflow with WorkflowModelView object

--- force_wfmanager/ui/setup/side_pane.py
+++ force_wfmanager/ui/setup/side_pane.py
@@ -6,8 +6,9 @@ from traits.api import (
 from traitsui.api import View, UItem, VGroup, UReadonly, TextEditor, Group
 
 from force_bdss.api import IFactoryRegistry
-from force_wfmanager.ui.setup.execution_layers.process_tree import ProcessTree
-from force_wfmanager.model.workflow import Workflow
+from force_wfmanager.ui.setup.process_tree import ProcessTree
+from force_bdss.api import Workflow
+from force_wfmanager.ui.setup.workflow_model_view import WorkflowModelView
 
 
 class SidePane(TraitsDockPane):
@@ -24,9 +25,7 @@ class SidePane(TraitsDockPane):
     # wfmanager_setup_task.WfManagerSetupTask.workflow_model>` changes.
     #: Listens to: :attr:`WfManagerSetupTask.workflow_model <force_wfmanager.\
     #: wfmanager_setup_task.WfManagerSetupTask.workflow_model>`
-    #:
-    #: Collated information about the total workflow
-    workflow_model = Instance(Workflow, allow_none=False
+    workflow_model = Instance(Workflow, allow_none=False)
 
     # -------------------
     # Required Attributes
@@ -40,7 +39,7 @@ class SidePane(TraitsDockPane):
     # ------------------
 
     #: An internal identifier for this pane
-    id = 'force_wfmanager.tree_pane'
+    id = 'force_wfmanager.side_pane'
 
     #: Name displayed as the title of this pane
     name = 'Workflow'
@@ -57,16 +56,19 @@ class SidePane(TraitsDockPane):
     #: Make the pane visible by default
     visible = True
 
+    #: Collated information about the total workflow
+    workflow_model_view = Instance(WorkflowModelView)
+
     #: Tree editor for the Process Model Workflow
     process_tree = Instance(ProcessTree)
 
     #: Enables or disables the workflow tree.
     ui_enabled = Bool(True)
 
-    #: MCO button for bringin up the MCO options
+    #: MCO button for bringing up the MCO options
     mco_button = Button('MCO')
 
-    #: MCO button for bringin up the Notification options
+    #: MCO button for bringing up the Notification options
     notification_button = Button('Notifications')
 
     #: The error message currently displayed in the UI.
@@ -96,59 +98,26 @@ class SidePane(TraitsDockPane):
         )
     )
 
+    def _workflow_model_view_default(self):
+        return WorkflowModelView(
+            model=self.workflow_model
+        )
+
     def _process_tree_default(self):
-        print('_process_tree_default called')
-        process_tree = ProcessTree(
-            model=self.workflow_model.process
-        return process_tree
+        return ProcessTree(
+            process_model_view=self.workflow_model_view.process_model_view,
+            _factory_registry=self.factory_registry,
+        )
 
-    def _error_message_default(self):
-        if self.process_model_view.error_message == '':
-            return ERROR_TEMPLATE.format(
-                "No errors for current workflow", "")
-        else:
-            error_list = self.process_model_view.error_message.split('\n')
-            body_strings = ''.join([SINGLE_ERROR.format(error)
-                                    for error in error_list])
-            return ERROR_TEMPLATE.format(
-                "Errors for current workflow:", body_strings)
-
-    @on_trait_change('process_model_view', post_init=True)
-    def update_process_tree(self):
-        print(self.process_model_view, self.factory_registry)
-        self.process_tree = self.workflow_model_view.process_model_view
-
-    @on_trait_change("process_tree.selected_error")
+    @on_trait_change("process_tree:selected_error")
     def _sync_setup_error(self):
+        print('side_pane _sync_setup_error called')
         self.setup_error = self.process_tree.selected_error
 
     def _mco_button_fired(self):
-        self.process_tree.selected_mv = self.process_model_view.mco_model_view
+        self.process_tree.selected_mv = self.workflow_model_view.mco_model_view
         self.process_tree.selected_factory_name = 'MCO'
 
     def _notification_button_fired(self):
-        self.process_tree.selected_mv = self.process_model_view.notification_listener_info
+        self.process_tree.selected_mv = self.workflow_model_view.notification_listener_info
         self.process_tree.selected_factory_name = 'Notification Listeners'
-
-
-ERROR_TEMPLATE = """
-    <html>
-    <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style type="text/css">
-            .container{{
-                width: 100%;
-                font-family: sans-serif;
-                display: block;
-            }}
-        </style>
-    </head>
-    <body>
-    <h4>{}</h4>
-        {}
-    </body>
-    </html>
-"""
-
-SINGLE_ERROR = r"""<p>{}<\p>"""
