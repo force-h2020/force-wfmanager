7bb8be39dbfc6af5035011ab502277f20931c116
James Johnson <jjohnson@enthought.com>
2019-08-07 18:07:56 +0100

    Tests for id parsing

--- force_wfmanager/ui/contributed_ui/tests/test_contributed_ui.py
+++ force_wfmanager/ui/contributed_ui/tests/test_contributed_ui.py
@@ -6,6 +6,7 @@ from pyface.ui.qt4.util.gui_test_assistant import GuiTestAssistant
 
 from force_wfmanager.tests.dummy_classes import DummyContributedUI2
 from force_wfmanager.ui import ContributedUIHandler
+from force_wfmanager.ui.contributed_ui.contributed_ui import search, parse_id
 
 
 class TestContributedUI(GuiTestAssistant, unittest.TestCase):
@@ -28,3 +29,40 @@ class TestContributedUI(GuiTestAssistant, unittest.TestCase):
     def test_required_plugins(self):
         expected = {"force.bdss.enthought.plugin.uitest.v2": 2}
         self.assertDictEqual(expected, self.ui.required_plugins)
+
+    def test_search_for_id(self):
+        input_dict = {
+            'mco': {
+                'id': 'mco_identifier'
+            },
+            'execution_layers': [
+                [
+                    {
+                        'id': 'datasource_id',
+                        'info': [1, 2, 3]
+                    },
+                    {
+                        'id': 'datasource_id2',
+                        'info': [4, 5, 6]
+                    },
+
+                ],
+                [
+                    {
+                        'id': 'datasource_id3',
+                        'info': [7, 8, 9]
+                    },
+                ]
+
+            ]
+        }
+        results = search(input_dict, search_term="id")
+        expected = [
+            'mco_identifier', 'datasource_id', 'datasource_id2',
+            'datasource_id3'
+        ]
+        self.assertListEqual(results, expected)
+
+    def test_parse_id_error(self):
+        with self.assertRaisesRegex(ValueError, "Unexpected plugin id:"):
+            parse_id("incorrectid")
