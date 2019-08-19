0670f2d631951420e30cf1d28e760b630550c044
flongford <flongford@enthought.com>
2019-08-19 14:46:00 +0100

    TST: including unit tests for contributing BasePlots via ServiceOffersPlugin

--- force_wfmanager/ui/__init__.py
+++ force_wfmanager/ui/__init__.py
@@ -3,3 +3,5 @@ from .contributed_ui.contributed_ui import ContributedUIHandler  # noqa
 from .contributed_ui.i_contributed_ui import IContributedUI  # noqa
 from .contributed_ui.ui_select_modal import UISelectModal  # noqa
 from .contributed_ui.ui_select_modal import UISelectHandler  # noqa
+from .review.plot import BasePlot # noqa
+from .review.i_base_plot import IBasePlot # noqa
---
+++ force_wfmanager/ui/review/tests/test_base_plot_plugin.py
@@ -0,0 +1,45 @@
+import unittest
+
+from envisage.core_plugin import CorePlugin
+from envisage.ui.tasks.tasks_plugin import TasksPlugin
+
+from force_bdss.api import plugin_id
+from force_bdss.core_plugins.service_offers_plugin import ServiceOffersPlugin
+from force_bdss.factory_registry_plugin import FactoryRegistryPlugin
+
+from force_wfmanager.plugins.wfmanager_plugin import WfManagerPlugin
+from force_wfmanager.tests.dummy_classes.dummy_factory import DummyFactory
+from force_wfmanager.ui import BasePlot, IBasePlot
+from force_wfmanager.wfmanager import WfManager
+
+
+class ExampleUIPlugin(ServiceOffersPlugin):
+
+    id = plugin_id("enthought", "test", 4)
+
+    def get_name(self):
+        return "Example"
+
+    def get_version(self):
+        return 4
+
+    def get_factory_classes(self):
+        return [DummyFactory]
+
+    def get_base_plots(self):
+        return (IBasePlot, [BasePlot, BasePlot])
+
+    def get_service_offer_factories(self):
+        return [self.get_base_plots()]
+
+
+class TestServiceOffersPlugin(unittest.TestCase):
+
+    def test_contributes_service(self):
+        plugins = [
+            CorePlugin(), TasksPlugin(), FactoryRegistryPlugin(),
+            WfManagerPlugin(workflow_file=None), ExampleUIPlugin()
+        ]
+        wfmanager = WfManager(plugins=plugins)
+        wfmanager.plugin_manager.start()
+        self.assertEqual(len(wfmanager.get_services(IBasePlot)), 2)
