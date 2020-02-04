d08c76f67820ef12e6fe00abeb9fcf8595fb5565
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-02-04 18:03:00 +0000

    TST: missing tests for base data view

---
+++ force_wfmanager/ui/review/tests/test_base_data_view.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from pyface.ui.qt4.util.gui_test_assistant import GuiTestAssistant
+from traits.testing.api import UnittestTools
+
+from force_wfmanager.ui.review.base_data_view import BaseDataView
+from force_wfmanager.model.analysis_model import AnalysisModel
+
+
+class TestAnyPlot(GuiTestAssistant, TestCase, UnittestTools):
+    def setUp(self):
+        super().setUp()
+        self.analysis_model = AnalysisModel()
+        self.plot = BaseDataView(analysis_model=self.analysis_model)
+
+    def test__update_data_arrays(self):
+        self.analysis_model.value_names = ("one", "two")
+        self.analysis_model.add_evaluation_step((1, 2))
+        self.analysis_model.add_evaluation_step((3, 4))
+
+        self.plot._update_data_arrays()
+        self.assertListEqual(
+            self.plot.data_arrays[0],
+            [
+                self.analysis_model.evaluation_steps[0][0],
+                self.analysis_model.evaluation_steps[1][0],
+            ],
+        )
+
+        self.assertListEqual(
+            self.plot.data_arrays[1],
+            [
+                self.analysis_model.evaluation_steps[0][1],
+                self.analysis_model.evaluation_steps[1][1],
+            ],
+        )
