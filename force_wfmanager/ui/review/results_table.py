e089b70d16f8db877ae6f241b056e59c66b06a7a
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-02-27 11:19:35 +0000

    MAINT: Update ResultsTable

--- force_wfmanager/ui/review/results_table.py
+++ force_wfmanager/ui/review/results_table.py
@@ -7,13 +7,13 @@ from traits.api import (
     Tuple,
     on_trait_change,
     Event,
-    Int
+    Int,
 )
 from traitsui.api import TabularEditor, UItem, View
 from traitsui.tabular_adapter import TabularAdapter
 from traitsui.table_column import ListColumn
 
-from force_wfmanager.model.analysis_model import AnalysisModel
+from force_wfmanager.model.new_analysis_model import AnalysisModel
 
 
 class ResultsTable(HasStrictTraits):
@@ -44,46 +44,46 @@ class ResultsTable(HasStrictTraits):
     # ----------
 
     #: Rows of the table_editor
-    rows = Property(
-        List(Tuple), depends_on='analysis_model.evaluation_steps'
-    )
+    rows = Property(List(Tuple), depends_on="analysis_model.evaluation_steps")
 
     #: Columns of the table_editor
-    columns = Property(
-        List(ListColumn), depends_on='analysis_model.value_names'
-    )
+    columns = Property(List(ListColumn), depends_on="analysis_model.header")
 
     def _get_rows(self):
         return self.analysis_model.evaluation_steps
 
     def _get_columns(self):
-        return [ListColumn(label=name, index=index)
-                for index, name
-                in enumerate(self.analysis_model.value_names)]
+        return [
+            ListColumn(label=name, index=index)
+            for index, name in enumerate(self.analysis_model.header)
+        ]
 
     # ----
     # View
     # ----
     def default_traits_view(self):
-        editor = TabularEditor(adapter=self.tabular_adapter,
-                               show_titles=True,
-                               selected="_selected_rows",
-                               auto_update=False,
-                               multi_select=True,
-                               scroll_to_row='_scroll_to_row',
-                               scroll_to_row_hint='visible',
-                               editable=False)
+        editor = TabularEditor(
+            adapter=self.tabular_adapter,
+            show_titles=True,
+            selected="_selected_rows",
+            auto_update=False,
+            multi_select=True,
+            scroll_to_row="_scroll_to_row",
+            scroll_to_row_hint="visible",
+            editable=False,
+        )
 
         return View(UItem("rows", editor=editor))
 
     # Response to model initialisation
-    @on_trait_change('analysis_model.value_names')
+    @on_trait_change("analysis_model.header")
     def _update_adapter(self):
-        self.tabular_adapter.columns = (
-            [name for name in self.analysis_model.value_names])
+        self.tabular_adapter.columns = [
+            name for name in self.analysis_model.header
+        ]
 
     # Response to model change
-    @on_trait_change('analysis_model.selected_step_indices')
+    @on_trait_change("analysis_model.selected_step_indices")
     def update_table(self):
         """ Updates the selected row in the table according to the model """
         if self.analysis_model.selected_step_indices is None:
@@ -95,7 +95,7 @@ class ResultsTable(HasStrictTraits):
             ]
 
     # Response to new selection by user in UI
-    @on_trait_change('_selected_rows[]')
+    @on_trait_change("_selected_rows[]")
     def update_model(self):
         """ Updates the model according to the selected row in the table """
         if not self._selected_rows:
